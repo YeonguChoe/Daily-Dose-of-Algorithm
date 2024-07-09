ceea71be49c0df2d2922f3526bb317429a4d3245
YeonguChoe <yeongu.choe@icloud.com>
2024-07-09 11:48:15 -0400

    Update 703.cpp

--- yeonguchoe/703.cpp
+++ yeonguchoe/703.cpp
@@ -1,29 +1,21 @@
-Space complexity: O(n)
-
-class KthLargest
-{
+class KthLargest {
 public:
+    int k_value = 0;
     priority_queue<int, vector<int>, greater<int>> min_heap;
-    int k_value;
 
-    KthLargest(int k, vector<int>& nums)
-    {
+    KthLargest(int k, vector<int>& nums) {
         k_value = k;
-        for (int num : nums)
-        {
+        for (int num : nums) {
             min_heap.push(num);
-            if (min_heap.size() > k)
-            {
-                min_heap.pop();
-            }
+        }
+        while (min_heap.size() > k_value) {
+            min_heap.pop();
         }
     }
 
-    int add(int val)
-    {
+    int add(int val) {
         min_heap.push(val);
-        while (min_heap.size() > k_value)
-        {
+        while (min_heap.size() > k_value) {
             min_heap.pop();
         }
         return min_heap.top();
