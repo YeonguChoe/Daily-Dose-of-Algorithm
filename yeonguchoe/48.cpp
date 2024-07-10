e783875337f627655aab9fc594c848de79b0253f
YeonguChoe <yeongu.choe@icloud.com>
2024-07-10 14:40:23 -0400

    Create 48.cpp

---
+++ yeonguchoe/48.cpp
@@ -0,0 +1,21 @@
+void rotate(vector<vector<int>>& matrix)
+{
+    int n = matrix.size();
+    // Transpose
+    for (int row = 0; row < n; row++)
+    {
+        for (int column = row + 1; column < n; column++)
+        {
+            swap(matrix[row][column], matrix[column][row]);
+        }
+    }
+
+    // Reverse each row
+    for (int row = 0; row < n; row++)
+    {
+        for (int column = 0; column < n / 2; column++)
+        {
+            swap(matrix[row][column], matrix[row][(n - 1) - column]);
+        }
+    }
+}
