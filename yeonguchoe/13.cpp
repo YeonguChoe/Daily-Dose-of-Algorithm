2039ada55f669955f30892359579b500839df708
YeonguChoe <yeongu.choe@icloud.com>
2024-08-22 21:10:53 -0400

    Update 13.cpp

--- yeonguchoe/13.cpp
+++ yeonguchoe/13.cpp
@@ -86,3 +86,26 @@ int romanToInt(string s)
     }
     return result;
 }
+
+// // Left Pass Soluion
+// int romanToInt(string s)
+// {
+//     unordered_map<char, int> v = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+//     int result = 0;
+//     int i = 0;
+//     while (i < s.size())
+//     {
+//         if (i <= s.size() - 2 and v[s[i]] < v[s[i + 1]])
+//         {
+//             result += v[s[i + 1]];
+//             result -= v[s[i]];
+//             i += 2;
+//         }
+//         else
+//         {
+//             result += v[s[i]];
+//             i += 1;
+//         }
+//     }
+//     return result;
+// }
