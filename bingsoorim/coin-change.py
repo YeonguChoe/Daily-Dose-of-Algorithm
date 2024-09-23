108d4c87280d37d145ed3dad57133571413687f4
Bingsoo <44685975+bingsoorim@users.noreply.github.com>
2024-09-23 19:55:06 -0400

    Create coin-change.py

---
+++ bingsoorim/coin-change.py
@@ -0,0 +1,12 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # classic dp-bottom up problem
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+        
+        return dp[amount] if dp[amount] != amount + 1 else -1
